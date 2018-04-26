2c25fcc1be206f8f589f9541563652d110713148
gongbo <gongbo84321@hotmail.com>
2018-04-26 22:25:32 +0200

    upload again without lfs

--- lib/coarsening.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d765f301fcfe2391ffc183dc2e06e4b12aae04b937d3157a6099355a1844411f
-size 8239
--- lib/models.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:83451cce446765741e19197c1c06df4c5183308245a242e7901d407e4056bb1d
-size 41218
